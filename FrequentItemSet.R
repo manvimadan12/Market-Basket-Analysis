b0efd620b974345a74076394a3d2dd3e23187517
Manvi Madan <manvimadan@outlook.com>
2020-04-11 21:34:31 +1200

    training using apriori
    THe results of rules for frequent dataset generated by apriori and eclat are stored in different variables and compared

--- FrequentItemSet.R
+++ FrequentItemSet.R
@@ -1,14 +1,19 @@
 library(arules)
   
-#eclat
+#data preprocessing
 dataset = read.csv('Market_Basket_Optimisation.csv')
 dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
 
 # Training Eclat on the dataset
-rules = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2))
+rules_eclat = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2))
 
 # Visualising the results
-inspect(sort(rules, by = 'support')[1:10])
+inspect(sort(rules_eclat, by = 'support')[1:10])
   
+# Training Apriori on the dataset
+rules_apriori = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
+
+# Visualising the results
+inspect(sort(rules_apriori, by = 'lift')[1:10])
