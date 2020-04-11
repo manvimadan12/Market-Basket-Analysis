abf4ba07747b9dfa898495b3f857760c15026571
Manvi Madan <manvimadan@outlook.com>
2020-04-11 21:05:32 +1200

    Frequent Item Set using Eclat

---
+++ FrequentItemSet.R
@@ -0,0 +1,9 @@
+ library(arules)
+  
+#eclat
+dataset = read.csv('Market_Basket_Optimisation.csv')
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+  
+  
